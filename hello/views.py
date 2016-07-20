26451c0f08dd0926a4c99439f8492c65747cb62c
Garnet Grimm <epicishness@live.com>
2016-07-20 19:10:57 -0400

    postgres test

--- hello/views.py
+++ hello/views.py
@@ -11,10 +11,10 @@ def index(request):
 
 def login(request):
 	if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = LoginForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
+		# create a form instance and populate it with data from the request:
+		form = LoginForm(request.POST)
+		# check whether it's valid:
+		if form.is_valid():
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
