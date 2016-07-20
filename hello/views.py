6b5339db9566db70fd18f16b87a4e5330256be42
Garnet Grimm <epicishness@live.com>
2016-07-20 19:08:26 -0400

    postgres test

--- hello/views.py
+++ hello/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
+from.forms import LoginForm
 
 # Create your views here.
 def index(request):
@@ -9,7 +10,20 @@ def index(request):
     return render(request, 'index.html')
 
 def login(request):
-	return render(request, 'base.html')
+	if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = LoginForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+			return HttpResponseRedirect('/thanks/')
+	    # if a GET (or any other method) we'll create a blank form
+		else:
+			form = NameForm()
+
+    return render(request, 'name.html', {'form': form})
 
 
 def db(request):
