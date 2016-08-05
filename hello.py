fa88d294b22bbddc77b112623adaed9743720c29
Garnet Grimm <epicishness@live.com>
2016-08-05 16:50:52 -0400

    db testing

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
 from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlechemy
+
 app = Flask(__name__, static_url_path = "", static_folder = "static")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLALchemy(app)
+
+#dont forget to add sqlaclhemy to the freeze thing
 
 @app.route('/')
 def hello_world():
