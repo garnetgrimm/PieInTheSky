f163e1d57bcf3e2c5ccd05509bc6920da803bf93
Garnet Grimm <epicishness@live.com>
2016-08-05 16:57:01 -0400

    db testing

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlechemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_url_path = "", static_folder = "static")
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
